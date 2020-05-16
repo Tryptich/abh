00603bde2edb51e801f2f1d9f0bfbd67884e3fb6
Steve Ruben <stevetuenkam@gmail.com>
2020-05-16 10:21:57 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
+// https://youtu.be/IXtZiuRCQsM
+//https://youtu.be/Hzs_8e3Xhhc?list=PLPhd673abXl7PPmhS4FmuTgPmOyv_xvYy
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { IsMobileProvider } from "./is-mobile";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <IsMobileProvider>
+       <App />
+    </IsMobileProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
